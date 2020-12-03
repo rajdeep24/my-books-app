75a5a7d5e03a2f7659579400a9393eadafa05211
Developer Raj <rajdeepkakar@gmail.com>
2020-12-03 15:17:50 -0500

    Imported mongoose to the server and defined a new mongoose connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-
+//Server connecting to localhost 3000 (front-end)
 const PORT = process.env.PORT || 3000;
+
+//Server connecting to the back-end database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my_books_db", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 app.get("/api/config", (req, res) => {
 	res.json({
