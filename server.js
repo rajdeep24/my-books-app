ed50975c1ca5158ab731ad7ca93991fe2b6c9288
Developer Raj <rajdeepkakar@gmail.com>
2020-12-03 15:24:04 -0500

    Added callback function to check if the connection to was mongoose db was sucessfull. Also added a callback to check if mongoose returned any errors while trying to connect. If any errors exist, the server will log the error to the console.

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my_books_db", {
 	useUnifiedTopology: true,
 });
 
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+	console.log("Mongoose successfully connected");
+});
+
+connection.on("error", (err) => {
+	console.log("Mongoose connection error: ", err);
+});
+
 app.get("/api/config", (req, res) => {
 	res.json({
 		success: true,
